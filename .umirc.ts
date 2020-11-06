bdb11f163efcaf56f23f1b8056408e81652cf8d8
oliver lou <1746299291@qq.com>
2020-11-07 00:50:59 +0800

    fix: 配置自动打开浏览器和端口号

--- .umirc.ts
+++ .umirc.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'umi';
+const openBrowser = require('open-browser-webpack-plugin');
 
 export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
   },
-  routes: [
-    { path: '/', component: '@/pages/index' },
-  ],
+  routes: [{ path: '/', component: '@/pages/index' }],
+  chainWebpack(config) {
+    config
+      .plugin('open-browser-webpack-plugin')
+      .use(openBrowser, [{ url: 'http://localhost:10000' }]); // 此处url与项目启动的url保持一致
+  },
 });
