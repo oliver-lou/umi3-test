4bcbfb6819102761a772532ad8424784673f07f0
oliver lou <1746299291@qq.com>
2020-11-17 09:28:14 +0800

    fix: 完成全局路径监听的函数的编写，初步测试可以正常使用

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import styles from './index.less';
+import { NavLink } from 'umi';
 
 export default () => {
-    return <div>index</div>;
+    return (
+        <ul>
+            <li>
+                <NavLink to="/dashboard">dashboard</NavLink>
+            </li>
+            <li>
+                <NavLink to="/test">test</NavLink>
+            </li>
+        </ul>
+    );
 };
